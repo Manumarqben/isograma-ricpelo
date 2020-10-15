7bb57284ba7b2667f2da4b77a8cdfa0047d790f2
Manuel Jesús Márquez Benítez <marqben@gmail.com>
2020-10-15 20:15:21 +0200

    servidor-isograma solucionado

--- Isograma.php
+++ Isograma.php
@@ -1,12 +1,21 @@
 <?php
 
-/*
- * Esto es sólo un ESQUELETO para el ejercicio de los "isogramas".
+/**
+ * @author Manuel Jesús Márquez Benítez <manueljesus.marquez@iesdonana.org>
+ * @copyright Copyright (c) 2020 Manuel Jesús Márquez Benítez
+ * @license 
+ */
+
+/**
+ * Comprueba si una cadena tiene algún caracter repetido
+ * 
+ * @param string $s La cadena a comprobar
+ * @return bool true si esta formado por caracteres unicos
+ *              false si tiene algún caracter repetido.
  */
 
 function esIsograma($s)
 {
-    //
-    // TU CÓDIGO VA AQUÍ
-    //
+    $s = mb_str_split(mb_ereg_replace('[- ]', '', mb_strtolower($s)));
+    return array_unique($s);
 }
